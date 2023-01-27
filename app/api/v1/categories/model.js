d2609deebb79292001eae6f7e209b13d4efe10c4
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2023-01-27 09:34:39 +0700

    fix models

--- app/api/v1/categories/model.js
+++ app/api/v1/categories/model.js
@@ -6,8 +6,8 @@ let categorySchema = Schema(
     name: {
       type: String,
       minlength: [3, "Panjang nama kategori minimal 3 karakter"],
-      maxlength: [20, "Panjang nama kategori minimal 3 karakter"],
-      required: true,
+      maxLength: [50, "Panjang nama kategori minimal 3 karakter"],
+      required: [true, "Nama kategori harus diisi"],
     },
   },
   { timestamps: true }
