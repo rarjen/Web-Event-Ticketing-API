5b0e3da889fe26396e961a38d3dd47440b9dcb19
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-01-28 23:03:05 +0700

    create router for events

---
+++ app/api/v1/events/router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const { create, index, find, update, destroy } = require("./controller");
+
+router.post("/events", create);
+router.get("/events", index);
+router.get("/events/:id", find);
+router.put("/events/:id", update);
+router.delete("/events/:id", destroy);
+
+module.exports = router;
