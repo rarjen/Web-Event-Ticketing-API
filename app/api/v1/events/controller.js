14afba2aedcc9b24199ee9ee63a6157f4b014a17
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-01-28 23:01:51 +0700

    use crud method for events from services

---
+++ app/api/v1/events/controller.js
@@ -0,0 +1,79 @@
+const {
+  getAllEvents,
+  getOneEvents,
+  updateEvents,
+  createEvents,
+  deleteEvents,
+} = require("../../../services/mongoose/events");
+
+const { StatusCodes } = require("http-status-codes");
+
+const create = async (req, res, next) => {
+  try {
+    const result = await createEvents(req);
+    return res.status(StatusCodes.CREATED).json({
+      status: true,
+      message: "Success create data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const index = async (req, res, next) => {
+  try {
+    const result = await getAllEvents(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get all data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const find = async (req, res, next) => {
+  try {
+    const result = await getOneEvents(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const update = async (req, res, next) => {
+  try {
+    const result = await updateEvents(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success update data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const destroy = async (req, res, next) => {
+  try {
+    const result = await deleteEvents(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success delete data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { create, index, find, update, destroy };
