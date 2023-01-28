c44dbffd19712ef7e933f04fc00a31f903d3837d
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-01-28 16:58:20 +0700

    create image router

--- app/api/v1/images/router.js
+++ app/api/v1/images/router.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-
 const { createImg } = require("./controller");
+const upload = require("../../../middlewares/multer");
 
-router.post("/upload", createImg);
+router.post("/images", upload.single("image"), createImg);
+
+module.exports = router;
