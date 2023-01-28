f716da4a0e84a2c481ab1ca553b0fb5cec7a850b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-01-28 22:56:01 +0700

    use events router

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const handleErrorMiddlware = require("./app/middlewares/handle-error");
 const categoriesRouter = require("./app/api/v1/categories/router");
 const imagesRouter = require("./app/api/v1/images/router");
 const talentsRouter = require("./app/api/v1/talents/router");
+const eventsRouter = require("./app/api/v1/events/router");
 
 const v1 = "/api/v1/cms";
 
@@ -36,6 +37,7 @@ app.get("/", (req, res) => {
 app.use(v1, categoriesRouter);
 app.use(v1, imagesRouter);
 app.use(v1, talentsRouter);
+app.use(v1, eventsRouter);
 
 app.use(notFoundMiddleware);
 app.use(handleErrorMiddlware);
