29d099d41b052ac53c23987caccad9c000905c52
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-01-28 16:59:29 +0700

    add checkingImage method and export

--- app/services/mongoose/images.js
+++ app/services/mongoose/images.js
@@ -1,4 +1,5 @@
 const Images = require("../../api/v1/images/model");
+const { NotFoundError } = require("../../errors");
 
 const generateUrlImage = async (req) => {
   const result = `upload/${req.file.filename}`;
@@ -16,4 +17,14 @@ const createImages = async (req) => {
   return result;
 };
 
-module.exports = { createImages, generateUrlImage };
+const checkingImage = async (id) => {
+  const result = await Images.findOne({ _id: id });
+
+  console.log(result);
+
+  if (!result) throw new NotFoundError(`Tidak ada Gambar dengan id: ${id}`);
+
+  return result;
+};
+
+module.exports = { createImages, generateUrlImage, checkingImage };
