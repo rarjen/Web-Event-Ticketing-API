26d5182bda0f74f581412773ffc7064e6ecb0182
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-01-31 19:58:30 +0700

    use middleware

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -2,13 +2,28 @@ const router = require("express").Router();
 const { create, index, find, update, destroy } = require("./controller");
 const {
   authenticateUser,
-  //   authorizeRoles,
+  authorizeRoles,
 } = require("../../../middlewares/auth");
 
 router.post("/categories", authenticateUser, create);
-router.get("/categories", authenticateUser, index);
-router.get("/categories/:id", find);
-router.put("/categories/:id", update);
-router.delete("/categories/:id", destroy);
+router.get("/categories", authenticateUser, authorizeRoles("organizer"), index);
+router.get(
+  "/categories/:id",
+  authenticateUser,
+  authorizeRoles("organizer"),
+  find
+);
+router.put(
+  "/categories/:id",
+  authenticateUser,
+  authorizeRoles("organizer"),
+  update
+);
+router.delete(
+  "/categories/:id",
+  authenticateUser,
+  authorizeRoles("organizer"),
+  destroy
+);
 
 module.exports = router;
