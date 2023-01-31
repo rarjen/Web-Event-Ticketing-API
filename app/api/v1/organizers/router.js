29e14ca2de12fcc04862ce274153539bd5cf214e
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-01-31 19:57:00 +0700

    fix router middleware create admin by organizer

--- app/api/v1/organizers/router.js
+++ app/api/v1/organizers/router.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 const {
   authenticateUser,
-  //   authorizeRoles,
+  authorizeRoles,
 } = require("../../../middlewares/auth");
 
 const { createCMSOrganizer, createCMSUser } = require("./controller");
 
 router.post("/organizers", createCMSOrganizer);
-router.post("/users", authenticateUser, createCMSUser);
+router.post(
+  "/users",
+  authenticateUser,
+  authorizeRoles("organizer"),
+  createCMSUser
+);
 
 module.exports = router;
