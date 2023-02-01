efd96b6bb24c5bbdab76b1d4227667769d837771
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-01 21:13:49 +0700

    use middleware

--- app/api/v1/images/router.js
+++ app/api/v1/images/router.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 
 const { createImg } = require("./controller");
+const {
+  authenticateUser,
+  authorizeRoles,
+} = require("../../../middlewares/auth");
 const upload = require("../../../middlewares/multer");
 
-router.post("/images", upload.single("image"), createImg);
+router.post("/images", upload.single("image"), authenticateUser, createImg);
 
 module.exports = router;
