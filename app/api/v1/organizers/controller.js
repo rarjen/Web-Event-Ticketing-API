9774eea3400e7de6d5c2ee3b6d7ff231303c00a5
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-01 21:50:49 +0700

    use method get all user for owner

--- app/api/v1/organizers/controller.js
+++ app/api/v1/organizers/controller.js
@@ -2,7 +2,22 @@ const { StatusCodes } = require("http-status-codes");
 const {
   createOrganizers,
   createUsers,
+  getAllUsers,
 } = require("../../../services/mongoose/users");
+
+const getCMSUsers = async (req, res, next) => {
+  try {
+    const result = await getAllUsers(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get data",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const createCMSOrganizer = async (req, res, next) => {
   try {
@@ -32,4 +47,4 @@ const createCMSUser = async (req, res, next) => {
   }
 };
 
-module.exports = { createCMSOrganizer, createCMSUser };
+module.exports = { createCMSOrganizer, createCMSUser, getCMSUsers };
