8bebfbaf61def1940b4949f81b9e643a699c0231
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-09 12:08:58 +0700

    make router for participants

--- app/api/v1/participants/router.js
+++ app/api/v1/participants/router.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
-const { register } = require("./controller");
+const {
+  signup,
+  singin,
+  allEvents,
+  allOrders,
+  oneEvent,
+} = require("./controller");
+const { authenticateParticipant } = require("../../../middlewares/auth");
 
-router.post("/auth/signup", register);
+router.post("/auth/signup", signup);
+router.post("/auth/signin", singin);
+router.get("/events", allEvents);
+router.get("/events/:id", oneEvent);
 
 module.exports = router;
