814bf7fadb9cf160b6a27b23907d45c0da38dcbb
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-09 12:08:30 +0700

    use method from participant services

--- app/api/v1/participants/controller.js
+++ app/api/v1/participants/controller.js
@@ -1,9 +1,13 @@
 const { StatusCodes } = require("http-status-codes");
 const {
   signupParticipants,
+  signInParticipant,
+  getAllEvents,
+  getAllOrders,
+  getOneEvent,
 } = require("../../../services/mongoose/participants");
 
-const register = async (req, res, next) => {
+const signup = async (req, res, next) => {
   try {
     const result = await signupParticipants(req);
 
@@ -17,4 +21,60 @@ const register = async (req, res, next) => {
   }
 };
 
-module.exports = { register };
+const singin = async (req, res, next) => {
+  try {
+    const result = await signInParticipant(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success login",
+      data: { token: result },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const allEvents = async (req, res, next) => {
+  try {
+    const result = await getAllEvents(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get all events",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const oneEvent = async (req, res, next) => {
+  try {
+    const result = await getOneEvent(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get event",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const allOrders = async (req, res, next) => {
+  try {
+    const result = await getAllOrders(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get all orders",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, singin, allEvents, oneEvent, allOrders };
