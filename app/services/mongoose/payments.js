21d34a17fc97c5720856e03df14570c49bd2bd0a
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-09 23:37:13 +0700

    create crud for payments

--- app/services/mongoose/payments.js
+++ app/services/mongoose/payments.js
@@ -24,7 +24,7 @@ const createPayment = async (req) => {
 
   const check = await Payments.findOne({ type, organizer: user.organizer });
 
-  if (check) throw new BadRequestError("Tipe pemayaran sudah ada");
+  if (check) throw new BadRequestError("Tipe pembayaran sudah ada");
 
   const result = await Payments.create({
     image,
@@ -35,6 +35,81 @@ const createPayment = async (req) => {
   return result;
 };
 
-const getOnePayments = async (req) => {};
+const getOnePayments = async (req) => {
+  const { id } = req.params;
+  const user = req.user;
 
-module.exports = { getAllPayments, createPayment, getOnePayments };
+  const result = await Payments.findOne({
+    _id: id,
+    organizer: user.organizer,
+  })
+    .populate({
+      path: "image",
+      select: "_id name",
+    })
+    .select("_id type status image");
+
+  if (!result) {
+    throw new NotFoundError(`Tidak ada pemabayaran dengan id: ${id}`);
+  }
+
+  return result;
+};
+
+const updatePayments = async (req) => {
+  const { id } = req.params;
+  const { type, image } = req.body;
+  const user = req.user;
+
+  await checkingImage(image);
+
+  const check = await Payments.findOne({
+    type,
+    organizer: user.organizer,
+    _id: { $ne: id },
+  });
+
+  if (check) throw new BadRequestError("Tipe pembayaran duplikat");
+
+  const result = await Payments.findOneAndUpdate(
+    { _id: id },
+    { type, image, organizer: user.organizer },
+    { new: true, runValidators: true }
+  );
+
+  if (!result) throw new NotFoundError(`Tidak ada pembayaran dengan id: ${id}`);
+
+  return result;
+};
+
+const deletePayments = async (req) => {
+  const { id } = req.params;
+  const user = req.user;
+
+  const result = await Payments.findOne({ _id: id, organizer: user.organizer });
+
+  if (!result) {
+    throw new NotFoundError(`Tidak ada tipe pembayaran dengan id: ${id}`);
+  }
+
+  await result.remove();
+
+  return result;
+};
+
+const checkingPayments = async (id) => {
+  const result = await Payments.findOne({ _id: id });
+
+  if (!result) throw new NotFoundError(`Tidak ada pembayaran dengan id: ${id}`);
+
+  return result;
+};
+
+module.exports = {
+  getAllPayments,
+  createPayment,
+  getOnePayments,
+  updatePayments,
+  deletePayments,
+  checkingPayments,
+};
