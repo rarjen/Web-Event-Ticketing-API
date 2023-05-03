a664fd99fc38214c37f68fdcd2a2e9cdc67f818f
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-03 16:02:04 +0700

    modify auth response

--- app/services/mongoose/auth.js
+++ app/services/mongoose/auth.js
@@ -1,7 +1,9 @@
 const Users = require("../../api/v1/users/model");
-const Participants = require("../../api/v1/participants/model");
 const { BadRequestError, UnauthorizedError } = require("../../errors");
-const { createTokenUser, createJWT } = require("../../utils");
+const { createTokenUser, createJWT, createRefreshJWT } = require("../../utils");
+const { createUserRefreshToken } = require("./userRefreshToken");
+
+const Participants = require("../../api/v1/participants/model");
 const jwt = require("jsonwebtoken");
 const { jwtSecret } = require("../../config");
 
@@ -20,12 +22,25 @@ const signin = async (req) => {
 
   // method comparePassword sudah dibuat di model user, bisa langsung panggil
   const isPasswordCorrect = await result.comparePassword(password);
+
   if (!isPasswordCorrect) {
     throw new UnauthorizedError("Email/Password salah");
   }
+
   const token = createJWT({ payload: createTokenUser(result) });
 
-  return { token, role: result.role };
+  const refreshToken = createRefreshJWT({ payload: createTokenUser(result) });
+  await createUserRefreshToken({
+    refreshToken,
+    user: result._id,
+  });
+
+  return {
+    token,
+    refreshToken,
+    role: result.role,
+    email: result.email,
+  };
 };
 
 const verify = async (req) => {
