b3957489844c2cdaef9898892773a3bfa00876ce
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-03 15:58:03 +0700

    add new config variables

--- app/config.js
+++ app/config.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 
-const { URL_MONGODB_DEV, JWT_SECRET_KEY, URL } = process.env;
+const {
+  URL_MONGODB_DEV,
+  JWT_SECRET_KEY,
+  JWT_EXPIRATION,
+  JWT_REFRESH_TOKEN_SECRET_KEY,
+  JWT_REFRESH_TOKEN_EXPIRATION,
+  URL,
+} = process.env;
 
 module.exports = {
   urlDB: URL_MONGODB_DEV,
-  jwtExpiration: "24h",
+  jwtExpiration: JWT_EXPIRATION,
   jwtSecret: JWT_SECRET_KEY,
+  jwtRefreshTokenSecret: JWT_REFRESH_TOKEN_SECRET_KEY,
+  jwtRefreshTokenExpiration: JWT_REFRESH_TOKEN_EXPIRATION,
   url: URL,
 };
