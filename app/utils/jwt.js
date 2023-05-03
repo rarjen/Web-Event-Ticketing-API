cc9d4104b3c3adf32835b08bc47663188b901d63
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-03 16:03:38 +0700

    modify jwt to refresh token

--- app/utils/jwt.js
+++ app/utils/jwt.js
@@ -1,24 +1,37 @@
-const { jwtSecret, jwtExpiration, jwtRefreshExpiration } = require("../config");
+const {
+  jwtSecret,
+  jwtExpiration,
+  jwtRefreshTokenSecret,
+  jwtRefreshTokenExpiration,
+} = require("../config");
 const jwt = require("jsonwebtoken");
 
+// token
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, jwtSecret, { expiresIn: jwtExpiration });
+  const token = jwt.sign(payload, jwtSecret, {
+    expiresIn: jwtExpiration,
+  });
 
   return token;
 };
 
-// const createRefreshToken = ({ payload }) => {
-//   const refeshToken = jwt.sign(payload, jwtSecret, {
-//     expiresIn: jwtExpiration,
-//   });
-
-//   return refeshToken;
-// };
-
 const isTokenValid = ({ token }) => jwt.verify(token, jwtSecret);
+
+const createRefreshJWT = ({ payload }) => {
+  const token = jwt.sign(payload, jwtRefreshTokenSecret, {
+    expiresIn: jwtRefreshTokenExpiration,
+  });
+
+  return token;
+};
+
+//refresh token
+const isTokenValidRefresh = ({ token }) =>
+  jwt.verify(token, jwtRefreshTokenSecret);
 
 module.exports = {
   createJWT,
-  //   createRefreshToken,
   isTokenValid,
+  createRefreshJWT,
+  isTokenValidRefresh,
 };
