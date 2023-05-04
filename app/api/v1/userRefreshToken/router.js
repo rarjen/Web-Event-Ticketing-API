9c69028b545ad84ddf1ce402e6f665c3f618fa3f
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-05-04 15:35:16 +0700

    modify route

--- app/api/v1/userRefreshToken/router.js
+++ app/api/v1/userRefreshToken/router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express();
 const { index } = require("./controller");
 
-router.get("/refresh-token/:refreshToken/:email", index);
+router.get("/refresh-token/:refreshToken", index);
 
 module.exports = router;
